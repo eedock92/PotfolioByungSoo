19c7505b2c66b17beaa3207ad324d7b50c3a4e4d
BYUNGSOO <eedock92@naver.com>
2021-06-18 21:52:03 +0900

    commit

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
-const app = express();
+const http = require('http');
+const fs = require('fs');
+//const app = express();
+
+const server = http.createServer((req, res) => {
+
+    if(request.method == 'GET' && http.request.url == '/'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream("./index.html").pipe(res)
+    }
+    
+});
 
 
-app.get('/', (req, res) => {
-    res.send('hello')
-})
+// app.get('/', (req, res) => {
+//     res.send('hello')
+// })
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function(){
+server.listen(port, function(){
     console.log('server on! http://loaclhost:' + port)
 })
 
