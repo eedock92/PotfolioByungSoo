5c94aaf002f703bb8391dbcc643272951c6e2afb
BYUNGSOO <eedock92@naver.com>
2021-06-19 13:28:31 +0900

    commit

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('/dist/index')
+app.get('', (req, res) => {
+    res.sendFile('./dist/index.html')
 })
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, function(){
     console.log('server on! http://loaclhost:' + port)
-})
+}) 
 
