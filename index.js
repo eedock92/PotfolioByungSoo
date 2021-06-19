24833ee7b48efdd3e6317a89524d3c64f5706cd1
BYUNGSOO <eedock92@naver.com>
2021-06-19 13:49:31 +0900

    commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 
-app.get('', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+app.get('/', (req, res) => {
+    res.render('index.html')
 })
 
 const port = process.env.PORT || 3000;
